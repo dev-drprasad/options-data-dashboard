af4bc9469844128b428738e102fab373d697f26c
Reddy Prasad D <dev.drprasad@aim.com>
2024-02-08 16:04:23 +0530

    📦 Setup github pages build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { URL, fileURLToPath } from "url";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-const bodyBackground = "#f0f2f5";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GH_PAGES ? "/options-data-dashboard/" : "",
+  build: {
+    outDir: process.env.GH_PAGES ? "./docs" : "./dist",
+  },
   resolve: {
     alias: [
       { find: "shared", replacement: fileURLToPath(new URL("./src/shared", import.meta.url)) },
